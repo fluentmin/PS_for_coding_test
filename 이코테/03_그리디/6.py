5e7a1a047e6fd9e9e65a1c11b78045ec8049f934
Changmin Yoo <fluentmin@gmail.com>
2023-03-16 08:55:38 +0900

    greedy_4

---
+++ 이코테/03_그리디/6.py
@@ -0,0 +1,14 @@
+# 만들 수 없는 금액(p.314)
+
+n = int(input())
+data = list(map(int, input().split()))
+
+data.sort()
+
+result = 1
+for i in data:
+    if(result < i):
+        break
+    result += i
+
+print(result)
